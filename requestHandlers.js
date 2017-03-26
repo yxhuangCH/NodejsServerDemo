b2af61417e5566a81724d8d881d9af5f6ab67019
yxhuang <yxhuang1012@gmail>
2017-03-26 17:10:35 +0800

    add child process

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,22 @@
 var exec = require("child_process").exec;
 
-function start(){
+function start(response){
 	console.log("Request handler 'start ' was called.");
 	var content = "empty";
 
 	exec("ls -lah", function(erro, stdout, stderr){
-		content = stdout;
+		response.writeHead(200, {"Content-Type":"text/plain"});
+		response.write(stdout);
+		response.end();
 	});
 	return content;
 }
 
-function upload(){
+function upload(response){
 	console.log("Request handler 'upload' was called.");
-	return "Hello Upload";
+	response.writeHead(200,{"Content-Type": "text/plain"});
+	response.write("Hello Upload");
+	response.end();
 }
 
 exports.start = start;
