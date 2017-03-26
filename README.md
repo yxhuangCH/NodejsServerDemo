bd3ed7d4505108b8b9454ff6840821ec8a739108
yxhuang <yxhuang1012@gmail>
2017-03-26 16:48:50 +0800

    add child process

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # NodejsServerDemo
 这是Node入门一书中的例子。 网站 http://www.nodebeginner.org/index-zh-cn.html
+
+
+1. 在 JavaScript 中， 对象就是一个键/值对的集合。
+
+2. Node.js 可以在不新增额外线程的情况下，依然可以对任务进行并行处理--- Node.js 是单线程的。
+	它通过事件轮训来实现并行操作。
+
+3. child_process() 模块实现非阻塞操作，exec().
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var http = require("http");
 var url = require("url");
 
-function start(route){
+function start(route, handle){
 	function onRequest(request, response){
 		var pathname = url.parse(request.url).pathname;
 		console.log("Requst for " + pathname + " received.");
 
-		route(pathname);
+		route(handle, pathname);
 
 		response.writeHead(200, {"Content-Type":"text/plain"});
-		response.write("Hello hyx");
+		var content = route(handle, pathname);
+		response.write(content);
 		response.end();
 	};
 
